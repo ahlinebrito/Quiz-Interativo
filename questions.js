11dadfe194f36f9df5a480d9b2acda516b35d9bb
ahlinebrito <120393377+ahlinebrito@users.noreply.github.com>
2023-11-07 21:11:33 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,42 @@
+export default[
+    {
+        question: "Qual é a raiz quadrada de 25?",
+        answers: [
+            {option: "5", correct: true},
+            {option: "7", correct: false},
+            {option: "9", correct: false},
+        ],
+    },
+
+    {
+        question: "Qual é a capital de Alagoas?",
+        answers: [
+            {option: "Cuiabá", correct: false},
+            {option: "Maceió", correct: true},
+            {option: "Palmas", correct: false},
+        ],
+
+    },
+    
+    {
+        question: "Quem dirigiu o filme Ilha do Medo?",
+        answers: [
+            {option: "Clint Eastwood", correct: false},
+            {option: "Quentin Tarantino", correct: false},
+            {option: "Martin Scorsese", correct: true},
+        ],
+
+    },
+    
+    {
+        question: "Qual é a fórmula química do Ácido Sulfúrico?",
+        answers: [
+            {option: "H2SO3", correct: false},
+            {option: "HCLO4", correct: false},
+            {option: "H2SO4", correct: true},
+        ],
+
+    }
+
+
+]
